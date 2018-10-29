5e2d0543bdc852fbef3a29b4f7ef58a23ae1876d
Ashwin Karangutkar <ashwin@BZ00977.local>
2018-10-29 15:04:13 +0530

    Firefox and Chrome Test

--- basics/ChromeTest.py
+++ basics/ChromeTest.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 class ChromeTest():
 
     def test(self):
         #Initializing Chrome
-        driver = webdriver.Chrome()
+        driver = webdriver.Chrome(ChromeDriverManager().install())
         #Opening Google Site
         driver.get("https://www.google.com")
 
