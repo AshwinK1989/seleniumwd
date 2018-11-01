2db0d21d68052b80befd195eedac8ae0e3ec99ce
AshwinK1989 <ashwin.karangutkar@gmail.com>
2018-11-01 19:08:55 +0530

    Added comments

--- unitestpackage/TestSuiteDemo.py
+++ unitestpackage/TestSuiteDemo.py
@@ -2,9 +2,12 @@ import unittest
 from unitestpackage.AssertDemo import AssertDemo
 from unitestpackage.UnitTestDemo import UnitTestDemo
 
+# First load the Test Case Classes
 tc1 = unittest.TestLoader().loadTestsFromTestCase(AssertDemo)
 tc2 = unittest.TestLoader().loadTestsFromTestCase(UnitTestDemo)
 
+# Create a Test Suite
 smoke_test = unittest.TestSuite([tc1, tc2])
 
+# Run the Suite
 unittest.TextTestRunner().run(smoke_test)
